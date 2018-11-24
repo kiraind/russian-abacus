6f7f751ab562fae8ff2398d9ecf33be87457f3c3
Aleksei Paksevatkin <kira.industries@gmail.com>
2018-11-25 02:27:19 +0300

    gh pages.........................

---
+++ docs/precache-manifest.0f58d4727cf6926e12dc49655fa1f41a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6984be3a4053ec1bb0056a26c7909677",
+    "url": "/russian-abacus/static/media/add.6984be3a.svg"
+  },
+  {
+    "revision": "58636be97418597d077a",
+    "url": "/russian-abacus/static/js/runtime~main.58636be9.js"
+  },
+  {
+    "revision": "3defcfe8d063977876d8",
+    "url": "/russian-abacus/static/js/main.3defcfe8.chunk.js"
+  },
+  {
+    "revision": "1f699859b81212936788",
+    "url": "/russian-abacus/static/js/1.1f699859.chunk.js"
+  },
+  {
+    "revision": "3defcfe8d063977876d8",
+    "url": "/russian-abacus/static/css/main.f7a7745f.chunk.css"
+  },
+  {
+    "revision": "27f00bed4768f04b7d11bb3f351ca4e4",
+    "url": "/russian-abacus/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3f8fbd79a06e8797fdebc26c14390d6a.js"
+  "/russian-abacus/precache-manifest.0f58d4727cf6926e12dc49655fa1f41a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/russian-abacus/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
