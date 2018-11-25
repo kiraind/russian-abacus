6e90bf567b41db39a7ee51034e57ce0b22e814b6
Aleksei Paksevatkin <kira.industries@gmail.com>
2018-11-26 01:30:51 +0300

    rebuild

---
+++ docs/build/precache-manifest.1cc062289595ba992bea8a8039cc41e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6984be3a4053ec1bb0056a26c7909677",
+    "url": "/russian-abacus/static/media/add.6984be3a.svg"
+  },
+  {
+    "revision": "58636be97418597d077a",
+    "url": "/russian-abacus/static/js/runtime~main.58636be9.js"
+  },
+  {
+    "revision": "67a0144417d1c47a9b0a",
+    "url": "/russian-abacus/static/js/main.67a01444.chunk.js"
+  },
+  {
+    "revision": "1f699859b81212936788",
+    "url": "/russian-abacus/static/js/1.1f699859.chunk.js"
+  },
+  {
+    "revision": "67a0144417d1c47a9b0a",
+    "url": "/russian-abacus/static/css/main.41c81c84.chunk.css"
+  },
+  {
+    "revision": "248e4fb0971d94dc3da7b05a8e88a9ab",
+    "url": "/russian-abacus/index.html"
+  }
+];
